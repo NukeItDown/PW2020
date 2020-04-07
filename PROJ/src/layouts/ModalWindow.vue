09b7c118bf7f5b1b2f08ad59900cc55e639fddc6
NukeItDown <43474518+NukeItDown@users.noreply.github.com>
2020-04-07 22:19:29 +0300

    added promocode

--- PROJ/src/layouts/ModalWindow.vue
+++ PROJ/src/layouts/ModalWindow.vue
@@ -3,12 +3,28 @@ import {mapGetters} from "vuex"
 export default {
   data(){
     return{
-
+      saved: 0,
+      promoCodeSale:1,
+      totalCost: 0,
+      code: ''
     }
   },
   props:['show'],
-  computed: mapGetters(["getTotalCost"]),
+  computed:{
+    price(){
+      return Math.round(this.getTotalCost*100*this.promoCodeSale)/100
+    }, 
+    ...mapGetters(["getTotalCost"])
+  },
   methods:{
+    verifyCode(code){
+      code.trim() == 'payless' ? this.promoCodeSale = 1 - 0.3: this.promoCodeSale = 1
+    }
+  },
+  updated(){
+    let cost = this.getTotalCost
+    this.totalCost = Math.round(cost*100*this.promoCodeSale)/100
+    this.saved = Math.round((cost-this.totalCost)*100)/100 
   }
 }
 </script>
@@ -17,7 +33,12 @@ export default {
 
     <div class="modal-content" >
       <span class="close" v-on:click="$emit('CloseModalWindow', !show)">&times;</span>
-      <p>Total:{{Math.round(getTotalCost*100)/100}}&#36;</p>
+      <div>Total:{{price}}&#36;</div>
+      <div class="offCost" v-if="saved != 0">SAVED: {{saved}}&#36;</div>
+      <input v-model="code" class="promoCode" type="text" placeholder="Promo code!"><button class="bth-submit" @click="verifyCode(code)">USE</button>
+      <form>
+
+      </form>
     </div>
 
   </div>
@@ -43,12 +64,15 @@ export default {
   padding: 20px;
   border: 1px solid #888;
   width: 80%;
+  position: relative;
 }
 
 /* The Close Button */
 .close {
   color: #aaaaaa;
-  float: right;
+  position: absolute;
+  top: 0px;
+  right: 5px;
   font-size: 28px;
   font-weight: bold;
 }
@@ -58,6 +82,10 @@ export default {
   color: #000;
   text-decoration: none;
   cursor: pointer;
+}
+.offCost{
+  background-color: rgba(231, 31, 31, 0.4);
+  border: 0.1vh solid rgba(255, 0, 0, 0.671);
 }
 
 </style>
